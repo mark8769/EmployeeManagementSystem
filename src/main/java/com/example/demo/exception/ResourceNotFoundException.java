f4d957899ecd68b93bbd5d2068463fa46d48e246
Mark Ortega <mortegaponce27@gmail.com>
2023-08-01 21:32:32 -0500

    Made custome exception for whenever Employee is not found in database.

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,2 +1,18 @@
-package com.example.demo.exception;public class ResourceNotFoundException {
+package com.example.demo.exception;
+
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Throw exception whenever resource(entity) is not found inside Database
+// Runtime Exception internally implements serializable interface
+@ResponseStatus(value= HttpStatus.NOT_FOUND) // Will return not found status to client
+public class ResourceNotFoundException extends RuntimeException{
+
+    // Serializable interface stuff.
+    private static final long serialVersionUID = 1l;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
