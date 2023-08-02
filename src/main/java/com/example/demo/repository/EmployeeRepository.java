a5e1ca52598d707649ccace20665c1f6ef0e2f91
Mark Ortega <mortegaponce27@gmail.com>
2023-08-01 21:33:16 -0500

    Added interface class for Repository methods that should be implemented in service layer.

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,2 +1,12 @@
-package com.example.demo.repository;public interface EmployeeRepository {
+package com.example.demo.repository;
+
+import com.example.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Extending JpaRepository exposes database core methods to use for accessing our repo.
+// E.g. Delete, Add, Update, Insert, FindAll, FindById, saveall, existsById, etc...
+// Allows for out of the box implementations, no boiler plate code to do this.
+// Pass in the Entity you want to use, and the Type being used for the entity id
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
