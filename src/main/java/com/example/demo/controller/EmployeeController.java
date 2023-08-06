ad3398b5794cf3aa1ec7c761245c701dcff7e400
Mark Ortega <mortegaponce27@gmail.com>
2023-08-06 15:33:21 -0500

    ./src/main/java/controller: Added @CrossOrigin annotation to controller. Fixes error from calling from localhost:3000 to localhost:8080 (cors issue).

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/") // Typically use v1 to denote that all rest apis are release 1
 // Standard url mapping in rest api mappings
