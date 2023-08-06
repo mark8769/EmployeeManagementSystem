e3b4dccf1b5c165e5b16a0d6975133ebfa79fd62
Mark Ortega <mortegaponce27@gmail.com>
2023-08-06 18:30:37 -0500

    react-frontend/src/App.js: Adding routes to different views. Using BrowserRouter, Route, Switch from react-router-dom.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; // css for footer
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// Router: For url paths
+// Route element:
+// https://www.freecodecamp.org/news/react-router-cheatsheet/#:~:text=The%20switch%20component%20looks%20through,one%20page%20at%20a%20time.
+// Switch element: Ensures that only one component(page/route) will render when going through the routes.
 import   ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,11 +12,20 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router> {/* Switches view for user? (best educated guess) */}
+        <div className="container">
+          <HeaderComponent /> {/* Outside of switch because header applies to all pages. */}
+            <div className="container">
+              <Switch>
+                {/* Configure routes here. http://localhost:3000/ */}
+                <Route path="/" component={ListEmployeeComponent}></Route>
+                <Route path="/employees" component={ListEmployeeComponent}></Route>
+                <ListEmployeeComponent />
+              </Switch>
+            </div>
+          <FooterComponent /> {/* Outside of swtich because footer applies to all pages. */}
+        </div>
+      </Router>
     </div>
   );
 }
