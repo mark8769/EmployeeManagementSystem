a97300079efff724b53fd38611b7708077077dfd
Mark Ortega <mortegaponce27@gmail.com>
2023-08-07 18:11:08 -0500

    react-frontend/src/app.js: Added another route for adding an employee form.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css'; // css for footer
 // https://stackoverflow.com/questions/69843615/switch-is-not-exported-from-react-router-dom
 // Use Routes instead of Switch
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter } from 'react-router-dom'
 // Router: For url paths
 // Route element:
 // https://www.freecodecamp.org/news/react-router-cheatsheet/#:~:text=The%20switch%20component%20looks%20through,one%20page%20at%20a%20time.
@@ -10,7 +10,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
+// https://blog.logrocket.com/react-router-v6-guide/
 function App() {
   return (
     <div>
@@ -19,11 +21,11 @@ function App() {
             <div className="container">
               <Routes>
                 {/* Configure routes here. http://localhost:3000/ */}
-                <Route path="/" component={ListEmployeeComponent} />
-                <Route path="/employees" component={ListEmployeeComponent} />
+                <Route path="/" exact element={<ListEmployeeComponent/>} />
+                <Route path="/employees" element={<ListEmployeeComponent/>} />
+                <Route path="/addEmployee" element={<CreateEmployeeComponent/>} />
               </Routes>
               {/* Get rid of this at some point (wrong way to do this) */}
-              <ListEmployeeComponent />
             </div>
           <FooterComponent /> {/* Outside of swtich because footer applies to all pages. */}
       </Router>
