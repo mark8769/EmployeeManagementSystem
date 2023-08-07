d9847f0ea7477de0e0b541f171dad9be2879c1d9
Mark Ortega <mortegaponce27@gmail.com>
2023-08-06 19:31:15 -0500

    react-frontend/src/app.js: Switch no longer used in react-router-dom; using Routes component now.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css'; // css for footer
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// https://stackoverflow.com/questions/69843615/switch-is-not-exported-from-react-router-dom
+// Use Routes instead of Switch
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // Router: For url paths
 // Route element:
 // https://www.freecodecamp.org/news/react-router-cheatsheet/#:~:text=The%20switch%20component%20looks%20through,one%20page%20at%20a%20time.
 // Switch element: Ensures that only one component(page/route) will render when going through the routes.
-import   ListEmployeeComponent from './components/ListEmployeeComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
@@ -13,18 +15,17 @@ function App() {
   return (
     <div>
       <Router> {/* Switches view for user? (best educated guess) */}
-        <div className="container">
           <HeaderComponent /> {/* Outside of switch because header applies to all pages. */}
             <div className="container">
-              <Switch>
+              <Routes>
                 {/* Configure routes here. http://localhost:3000/ */}
-                <Route path="/" component={ListEmployeeComponent}></Route>
-                <Route path="/employees" component={ListEmployeeComponent}></Route>
-                <ListEmployeeComponent />
-              </Switch>
+                <Route path="/" component={ListEmployeeComponent} />
+                <Route path="/employees" component={ListEmployeeComponent} />
+              </Routes>
+              {/* Get rid of this at some point (wrong way to do this) */}
+              <ListEmployeeComponent />
             </div>
           <FooterComponent /> {/* Outside of swtich because footer applies to all pages. */}
-        </div>
       </Router>
     </div>
   );
