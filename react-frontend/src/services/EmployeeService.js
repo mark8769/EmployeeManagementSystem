4f185a40fd059a0b47cf9b76f96989a08a1387cd
Mark Ortega <mortegaponce27@gmail.com>
2023-08-09 16:03:03 -0500

    EmployeeService: Added method to save employee to database using axios, calling spring backend.

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees"
+const EMPLOYEE_API_ADD = "http://localhost:8080/api/v1/addEmployee"
 class EmployeeService {
 
     // Returning results from get api call.
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_ADD, employee);
     }
 }
 
