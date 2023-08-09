f1f8440eba0eabb3927729f4ddde826fd0440e53
Mark Ortega <mortegaponce27@gmail.com>
2023-08-09 15:57:55 -0500

    react-frontend/src/app.js: Configured routing to work with react-router v5 instead of v6.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css'; // css for footer
 // https://stackoverflow.com/questions/69843615/switch-is-not-exported-from-react-router-dom
 // Use Routes instead of Switch
-import { BrowserRouter as Router, Route, Routes, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes,  Switch } from 'react-router-dom'
 // Router: For url paths
 // Route element:
 // https://www.freecodecamp.org/news/react-router-cheatsheet/#:~:text=The%20switch%20component%20looks%20through,one%20page%20at%20a%20time.
@@ -19,12 +19,17 @@ function App() {
       <Router> {/* Switches view for user? (best educated guess) */}
           <HeaderComponent /> {/* Outside of switch because header applies to all pages. */}
             <div className="container">
-              <Routes>
+              {/* <Routes> */}
+              <Switch>
                 {/* Configure routes here. http://localhost:3000/ */}
-                <Route path="/" exact element={<ListEmployeeComponent/>} />
+                {/* <Route path="/" exact element={<ListEmployeeComponent/>} />
                 <Route path="/employees" element={<ListEmployeeComponent/>} />
-                <Route path="/addEmployee" element={<CreateEmployeeComponent/>} />
-              </Routes>
+                <Route path="/addEmployee" element={<CreateEmployeeComponent/>} /> */}
+                <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                <Route path = "/employees" exact component = {ListEmployeeComponent}></Route>
+                <Route path = "/addEmployee" exact component = {CreateEmployeeComponent}></Route>
+              </Switch>
+              {/* </Routes> */}
               {/* Get rid of this at some point (wrong way to do this) */}
             </div>
           <FooterComponent /> {/* Outside of swtich because footer applies to all pages. */}
