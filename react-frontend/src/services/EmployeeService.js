717a084423e919f8b660f9fceec7ba065ce2d262
Mark Ortega <mortegaponce27@gmail.com>
2023-08-15 21:16:32 -0500

    react-frontend/src/services/EmployeeService.js: Added method to update employee. Calling spring backend.

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,15 +1,23 @@
 import axios from 'axios'
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees"
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/"
 const EMPLOYEE_API_ADD = "http://localhost:8080/api/v1/addEmployee"
+
 class EmployeeService {
 
     // Returning results from get api call.
     getEmployees(){
-        return axios.get(EMPLOYEE_API_BASE_URL);
+        return axios.get(EMPLOYEE_API_BASE_URL + "employees");
     }
     createEmployee(employee){
         return axios.post(EMPLOYEE_API_ADD, employee);
+    }
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + "employees/" + employeeId)
+    }
+    updateEmployee(employee, id){
+        // Return a promise
+        return axios.put(EMPLOYEE_API_BASE_URL + `updateEmployee/${id}`, employee);
     }
 }
 
